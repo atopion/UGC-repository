eb036d8600e5077c2e40db273e62d7a9a930ca9f
atopion <atopion@outlook.com>
2020-04-03 17:13:04 +0200

    Fixed redirect port.

--- src/main/java/com/atopion/UGC_repository/WebConfig.java
+++ src/main/java/com/atopion/UGC_repository/WebConfig.java
@@ -173,7 +173,7 @@ public class WebConfig implements WebMvcConfigurer {
     private Connector getHTTPConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(8080);
         connector.setSecure(false);
         connector.setRedirectPort(443);
         return connector;
